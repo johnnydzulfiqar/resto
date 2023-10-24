d119d62b353585cdeecba2e9bc8ec2a00211c2e3
M Naufa Dzulfiqar <muhammadnaufa007@gmail.com>
2023-10-24 19:19:53 +0700

    first

---
+++ README.md
@@ -0,0 +1,11 @@
+Documentation
+composer update
+
+cp .env.example .env
+
+php artisan key:generate
+
+php artisan migrate --seed
+
+php artisan serve
+```
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'admin',
+                'username' => 'admin',
+                'password' => bcrypt('12345'),
+                'pass' => '12345',
+                'role' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'kasir',
+                'username' => 'kasir',
+                'password' => bcrypt('12345'),
+                'pass' => '12345',
+                'role' => 'kasir',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'manager',
+                'username' => 'manager',
+                'password' => bcrypt('12345'),
+                'pass' => '12345',
+                'role' => 'manajer',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'user',
+                'username' => 'user',
+                'password' => bcrypt('12345'),
+                'pass' => '12345',
+                'role' => 'pelanggan',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
