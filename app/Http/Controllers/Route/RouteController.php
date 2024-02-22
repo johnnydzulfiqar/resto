3b03130bbed9a6ee75e6a3b873e48c36aeadb81e
M Naufa Dzulfiqar <muhammadnaufa007@gmail.com>
2024-02-22 22:31:01 +0700

    update

--- app/Http/Controllers/Route/RouteController.php
+++ app/Http/Controllers/Route/RouteController.php
@@ -71,13 +71,18 @@ class RouteController extends Controller
 
         $data_keseluruhan = Transaksi::where('user_id', $user_id)->whereDate('created_at', '<', date('Y-m-d'))->get();
 
-        $rekap_transaksi = RekapTransaksi::where('user_id', $user_id)->get();
+        $menus = Menu::get('nama_menu');
+        $total_menu = Menu::all()->count();
 
-        if ($rekap_transaksi->count() > 0) {
-            $total_bayar = $rekap_transaksi[0]->total_bayar;
-        } else {
-            $total_bayar = 0;
-        }
+        $today = now()->format('yy-mm-dd');
+        // $AgoDate= now()->subWeek()->format('Y-m-d');
+        $AgoDate = now()->subDays()->format('Y-m-d');
+        $in_a_week = Transaksi::where('created_at', '>=', $AgoDate);
+
+        $total_transaksi = $in_a_week->count();
+
+        $total_bayar = $in_a_week->sum('total_harga');
+
 
         return view('pelanggan.transaksi.index', compact('data_hari_ini', 'data_keseluruhan', 'total_bayar'));
     }
